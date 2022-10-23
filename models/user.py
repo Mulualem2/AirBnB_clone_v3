54b15a407aed1f6c089fee03f12ca2805f4ff2a6
Mulualem2 <mulualemasfaw2321@gmail.com>
2022-10-23 00:48:27 -0700

    updating user.py

--- models/user.py
+++ models/user.py
@@ -6,7 +6,7 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-
+import hashlib
 
 class User(BaseModel, Base):
     """Representation of a user """
@@ -27,3 +27,6 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+        if self.password is not None:
+            hash_word = hashlib.md5(self.password.encode())
+            self.password = hash_word.hexdigest()
